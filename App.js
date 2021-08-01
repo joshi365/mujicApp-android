71e951c92aa03e071007c85a7a23bce950a2ab4a
joshi365 <sumit.joshi.365@gmail.com>
2021-08-01 19:49:07 +0530

    initial setup

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+      <Text>HELLLOOOOO</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
